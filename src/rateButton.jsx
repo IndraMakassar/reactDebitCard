c4efdca5607910d541958a942d2e6cf96b094632
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-11-22 16:15:51 +0800

    change how the rate button change color

--- src/rateButton.jsx
+++ src/rateButton.jsx
@@ -1,6 +1,6 @@
 function RateButton({ value, onSquareClick, pressed}) {
     return (
-        <button className={pressed ? "w-12 h-12 m-4 border-2 bg-cyan-800" : "w-12 h-12 m-4 border-2"} onClick={onSquareClick}>{value}</button>
+        <button className={pressed ? "w-12 h-12 border-2 bg-cyan-800 text-blue-50" : "w-12 h-12 border-2"} onClick={onSquareClick}>{value}</button>
     );
 }
 
--- src/rateButtons.jsx
+++ src/rateButtons.jsx
@@ -6,18 +6,23 @@ function RateButtons() {
 
     function handleClick(i) {
         const newPressed = Array(5).fill(false);
-        newPressed[i] = true;
+        for (let j = 1; j <= i; j++) {
+            newPressed[j] = true;
+        }
         setPressed(newPressed);
     }
 
     return (
-        <>
-            <RateButton value={"1"} pressed={pressed[1]} onSquareClick={() => handleClick(1)}/>
-            <RateButton value={"2"} pressed={pressed[2]} onSquareClick={() => handleClick(2)}/>
-            <RateButton value={"3"} pressed={pressed[3]} onSquareClick={() => handleClick(3)}/>
-            <RateButton value={"4"} pressed={pressed[4]} onSquareClick={() => handleClick(4)}/>
-            <RateButton value={"5"} pressed={pressed[5]} onSquareClick={() => handleClick(5)}/>
-        </>
+        <div className={"w-96 h-20 flex flex-col text-center justify-between"}>
+            <h1 className={"text-xl"}>Are you satisfied?</h1>
+            <div className={"flex flex-row justify-around"}>
+                <RateButton value={"1"} pressed={pressed[1]} onSquareClick={() => handleClick(1)}/>
+                <RateButton value={"2"} pressed={pressed[2]} onSquareClick={() => handleClick(2)}/>
+                <RateButton value={"3"} pressed={pressed[3]} onSquareClick={() => handleClick(3)}/>
+                <RateButton value={"4"} pressed={pressed[4]} onSquareClick={() => handleClick(4)}/>
+                <RateButton value={"5"} pressed={pressed[5]} onSquareClick={() => handleClick(5)}/>
+            </div>
+        </div>
     );
 }
 
